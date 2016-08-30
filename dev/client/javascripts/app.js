5935682e7447dfca3bf1e48cab12e22090de1681
betterbasics <gil@betterbasics.com>
2016-08-31 06:06:33 +0800

    created social-media.json data

--- dev/client/javascripts/app.js
+++ dev/client/javascripts/app.js
@@ -1,114 +1,24 @@
-var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($rootScope) {
-	$rootScope.authenticated = false;
-	$rootScope.current_user = '';
+var app = angular.module('devggApp', ['ngRoute', 'ngResource']);
 
-	$rootScope.signout = function(){
-    	$http.get('auth/signout');
-    	$rootScope.authenticated = false;
-    	$rootScope.current_user = '';
-	};
-});
-
-app.config(function($routeProvider){
-	$routeProvider
-		//the timeline display
-		.when('/', {
-			templateUrl: 'main.html',
-			controller: 'mainController'
-		})
-		//the login display
-		.when('/login', {
-			templateUrl: 'login.html',
-			controller: 'authController'
-		})
-		//the signup display
-		.when('/register', {
-			templateUrl: 'register.html',
-			controller: 'authController'
-		})
-		//the products display
-		.when('/products', {
-			templateUrl: 'register.html',
-			controller: 'authController'
-		});
-});
-
-app.factory('postService', function($resource){
-//    console.log($resource('/api/posts/:id'))
-	return $resource('/api/posts/:id');
-});
-
-app.controller('mainController', function(postService, $scope, $rootScope){
-	$scope.posts = postService.query();
-//    console.log($scope.posts);
-	$scope.newPost = {created_by: '', text: '', created_at: ''};
-
-	$scope.post = function() {
-	  $scope.newPost.created_by = $rootScope.current_user;
-	  $scope.newPost.created_at = Date.now();
-	  postService.save($scope.newPost, function(){
-	    $scope.posts = postService.query();
-	    $scope.newPost = {created_by: '', text: '', created_at: ''};
-	  });
-	};
-});
-
-app.controller('authController', function($scope, $http, $rootScope, $location){
-  $scope.user = {username: '', password: ''};
-  $scope.error_message = '';
-
-  $scope.login = function(){
-    $http.post('/auth/login', $scope.user).success(function(data){
-      if(data.state == 'success'){
-        $rootScope.authenticated = true;
-        $rootScope.current_user = data.user.username;
-        $location.path('/');
-      }
-      else{
-        $scope.error_message = data.message;
-      }
+app.controller('socialMediaController',function($scope, $http){
+    $scope.accounts = {};
+    $http.get("client/json/social-media.json").then(function(res) {
+         $scope.accounts = res.data;
     });
-  };
+//    console.log($scope.accounts);
+});
 
-  $scope.register = function(){
-    $http.post('/auth/signup', $scope.user).success(function(data){
-      if(data.state == 'success'){
-        $rootScope.authenticated = true;
-        $rootScope.current_user = data.user.username;
-        $location.path('/');
-      }
-      else{
-        $scope.error_message = data.message;
-      }
-    });
-  };
+app.controller('menuController',function($scope, $http){
+    $scope.menu = {};
 });
 
 /*
-app.controller('socialMediaController',function($scope){
-    $scope.accounts = ["twitter","facebook","instagram","youtube"];
-});
-*/
-
-app.factory('socialAccountService', function($resource){
-	return $resource('/api/socials/:id');
-});
-
 app.controller('socialMediaController',function(socialAccountService, $scope, $rootScope){
 	$scope.accounts = socialAccountService.query();
     console.log($scope.accounts);
 //    $scope.accounts = ["twitter","facebook","instagram","youtube"];
 });
+*/
 
-app.factory('menuService', function($resource){
-	return $resource('/api/menus/:id');
-});
 
-app.controller('menuController',function(menuService, $scope, $rootScope, $location){
-	$scope.menu = menuService.query();
-    console.log($scope.menu);
-    $scope.isActive = function(destination){
-        return destination === $location.path();
-    }
-});
 
